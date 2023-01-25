f93a5f5ef429a79fe17395da8ca47dcb2d2a23b4
Leandro <98779007+LeandroSimoes42@users.noreply.github.com>
2023-01-25 20:07:03 -0300

    styles: implementation of animation on page screen

--- src/presentation/common/Splash/index.tsx
+++ src/presentation/common/Splash/index.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Animated, {
+  Extrapolate,
+  interpolate,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import * as S from './styles';
 
 const Splash = () => {
+  const splashAnimation = useSharedValue(0);
+
+  const LogoStyleAnimated = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP,
+          ),
+        },
+      ],
+    };
+  });
+
+  const startApp = () => {};
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(
+      50,
+      {
+        duration: 1000,
+      },
+      () => {
+        'worklet';
+        runOnJS(startApp)();
+      },
+    );
+  }, []);
+
   return (
     <S.Container>
-      <S.Title>Intelibank</S.Title>
+      <Animated.View style={[LogoStyleAnimated]}>
+        <S.Title>Intelibank</S.Title>
+      </Animated.View>
     </S.Container>
   );
 };
